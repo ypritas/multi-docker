7d1fa059236b2b85d6b3a2ca71b810a770db8654
Tomas <tomas.bartnykas@gmail.com>
2020-07-09 13:09:54 +0200

    Added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t ypritas/multi-client:latest -t ypritas/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t ypritas/multi-server:latest -t ypritas/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t ypritas/multi-worker:latest -t ypritas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push ypritas/multi-client:latest
 docker push ypritas/multi-server:latest
 docker push ypritas/multi-worker:latest
@@ -11,5 +12,5 @@ docker push ypritas/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ypritas/multi-server:$SHA
-kubectl set image deployments/client-deployment server=ypritas/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=ypritas/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=ypritas/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ypritas/multi-worker:$SHA
